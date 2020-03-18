5ab73b679e8e3a435c91f43e5d44d88c07cdcd93
kgeor <kgeorgesilva97@gmail.com>
2020-03-18 12:44:19 +0530

    Added the files to deploy

---
+++ build/precache-manifest.e48a87dabf8aad28188c0cca61780e8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf4b86b2e137b23b9d6746f4a1502e2c",
+    "url": "/QuizApp_ReactJS/index.html"
+  },
+  {
+    "revision": "b3c9d779035384f45da1",
+    "url": "/QuizApp_ReactJS/static/css/main.d261337b.chunk.css"
+  },
+  {
+    "revision": "1246c15d55231938d7a5",
+    "url": "/QuizApp_ReactJS/static/js/2.868b95b3.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/QuizApp_ReactJS/static/js/2.868b95b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3c9d779035384f45da1",
+    "url": "/QuizApp_ReactJS/static/js/main.58d107ef.chunk.js"
+  },
+  {
+    "revision": "5ae45fc4631547e1dfda",
+    "url": "/QuizApp_ReactJS/static/js/runtime-main.62f6342d.js"
+  }
+]);
